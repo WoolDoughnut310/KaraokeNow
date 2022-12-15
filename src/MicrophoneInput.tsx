9000609820bc1583195aa7f38bfd66b1acc7e2a6
WoolDoughnut310 <wooldoughnutspi@outlook.com>
2022-12-15 20:47:31 +0000

    Added a recording progress animation

--- src/MicrophoneInput.tsx
+++ src/MicrophoneInput.tsx
@@ -1,19 +1,33 @@
-import React, { useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { useReactMediaRecorder } from "react-media-recorder";
-import { Mic, MicOff, Activity } from "react-feather";
+import { Mic, Activity } from "react-feather";
 import axios from "axios";
 import { useSetAtom } from "jotai";
-import { errorAtom } from "./store";
+import { errorAtom, lyricsAtom, titleAtom } from "./store";
+
+const RECORD_DURATION = 15000;
 
 export default function MicrophoneInput() {
     const setError = useSetAtom(errorAtom);
+    const setLyrics = useSetAtom(lyricsAtom);
+    const setTitle = useSetAtom(titleAtom);
     const { status, startRecording, stopRecording } = useReactMediaRecorder({
         audio: true,
         onStop: (_url, blob) => {
             onSubmit(blob);
         },
     });
-    console.log(status);
+    const [endTime, setEndTime] = useState(0);
+    const [progressHeight, setProgressHeight] = useState(100);
+
+    const updateProgress = () => {
+        const recordPercentage =
+            Math.max(0, endTime - new Date().getTime()) / RECORD_DURATION;
+        setProgressHeight(
+            recordPercentage === 0 ? 100 : recordPercentage * 100
+        );
+        window.requestAnimationFrame(updateProgress);
+    };
 
     const onClick = async () => {
         if (status === "recording") {
@@ -22,10 +36,23 @@ export default function MicrophoneInput() {
 
         await startRecording();
 
+        setEndTime(new Date().getTime() + RECORD_DURATION);
+
         await new Promise((resolve) => {
-            setTimeout(resolve, 20000);
+            setTimeout(resolve, RECORD_DURATION);
         });
+
+        stopRecording();
     };
+
+    useEffect(() => {
+        if (endTime === 0) return;
+        const request = window.requestAnimationFrame(updateProgress);
+
+        return () => {
+            window.cancelAnimationFrame(request);
+        };
+    }, [endTime]);
 
     const onSubmit = async (blob: Blob) => {
         const body = new FormData();
@@ -38,10 +65,13 @@ export default function MicrophoneInput() {
 
             const trackISRC = response.data;
 
-            response = await axios.get("/api/find-lyrics", {params: {isrc: trackISRC}});
-            const lyrics = response.data;
+            response = await axios.get("/api/find-lyrics", {
+                params: { isrc: trackISRC },
+            });
+            const { title, lyrics } = response.data;
 
-            setLyrics(lyrics)
+            setTitle(title);
+            setLyrics(lyrics);
         } catch (err) {
             setError((err as Error).message);
         }
@@ -52,11 +82,17 @@ export default function MicrophoneInput() {
     return (
         <button
             type="button"
-            className="bg-slate-500 rounded-2xl border-gray-300 border-2 p-7"
+            className="bg-indigo-300 rounded-2xl border-gray-300 border-2 p-7 relative"
             onClick={onClick}
             disabled={status === "recording"}
         >
-            <Icon className="w-12 h-12" />
+            <div
+                style={{
+                    height: `${progressHeight}%`,
+                }}
+                className="absolute rounded-2xl w-full bg-indigo-500 bottom-0 left-0"
+            />
+            <Icon className="relative z-10 w-12 h-12" />
         </button>
     );
 }
