56e2e4dcc6a2cfc1f91fece044e7a295115f3ab7
WoolDoughnut310 <wooldoughnutspi@outlook.com>
2023-09-23 14:43:50 +0100

    feat: format code

--- api/_authClient.ts
+++ api/_authClient.ts
@@ -1,13 +1,15 @@
 import { AuthorizationCode } from "simple-oauth2";
 
 export const client = new AuthorizationCode({
-    client: {
-        id: process.env.GENIUS_CLIENT_ID as string,
-        secret: process.env.GENIUS_CLIENT_SECRET as string,
-    },
-    auth: {
-        tokenHost: process.env.GENIUS_HOST as string,
-        tokenPath: "/oauth/token",
-        authorizePath: "/oauth/authorize",
-    },
+  client: {
+    id: process.env.GENIUS_CLIENT_ID as string,
+    secret: process.env.GENIUS_CLIENT_SECRET as string,
+  },
+  auth: {
+    tokenHost: process.env.GENIUS_HOST as string,
+    tokenPath: "/oauth/token",
+    authorizePath: "/oauth/authorize",
+  },
 });
+
+export const redirectUri = "http://localhost:3000/auth-callback";
